0f8420becaa5235a3929a0a39fab287aba675d9f
LarryWisherMan <MikeRow@upkep.net>
2025-01-04 15:30:16 -0800

    Refactor cache classes and add new interfaces
    Refactored `MonitorCache` and `WakeOnLanResultCache` to implement
    specific interfaces (`IMonitorCache` and `IWakeOnLanResultCache`),
    adding null checks, new methods (`GetAll`, `Remove`, `Clear`), and
    updating `GetAllKeys` method. Updated `assemblyinfo.cs` to version
    "1.0.0.0". Added new interfaces `IMonitorCache`, `IProxyRequestProcessor`,
    and `IWakeOnLanResultCache`. Implemented `ProxyRequestProcessor` class
    with `ProcessProxyRequestsAsync` method.

---
+++ src/WakeOnLanLibrary/Application/Interfaces/IWakeOnLanResultCache.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using WakeOnLanLibrary.Application.Models;
+using WakeOnLanLibrary.Infrastructure.Caching;
+
+namespace WakeOnLanLibrary.Application.Interfaces
+{
+    public interface IWakeOnLanResultCache : ICache<string, WakeOnLanReturn>
+    {
+        /// <summary>
+        /// Retrieves all results from the cache, optionally sorted.
+        /// </summary>
+        /// <param name="sortDescending">Whether to sort the results in descending order.</param>
+        /// <returns>A sorted or unsorted collection of WakeOnLanReturn objects.</returns>
+        IEnumerable<WakeOnLanReturn> GetAllResults(bool sortDescending = false);
+    }
+}
