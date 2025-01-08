c73e1629e683b035aade7313783a9244747f0c7e
LarryWisherMan <MikeRow@upkep.net>
2025-01-08 07:57:34 -0800

    Add TestComputer class and validation unit tests
    Introduce TestComputer class in WakeOnLanLibrary.Tests.TestObjects namespace, inheriting from Computer. Add unit tests in GeneralComputerValidationStrategyTests using AutoMock from Autofac.Extras.Moq to validate GeneralComputerValidationStrategy behavior.

---
+++ tests/WakeOnLanLibrary.Tests/TestObjects/TestComputer.cs
@@ -0,0 +1,10 @@
+﻿using WakeOnLanLibrary.Core.Entities;
+
+namespace WakeOnLanLibrary.Tests.TestObjects
+{
+    public class TestComputer : Computer
+    {
+        // You can override any abstract members or properties if needed
+    }
+
+}
